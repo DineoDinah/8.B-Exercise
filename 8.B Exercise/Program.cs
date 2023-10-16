135b0ab4c9d3ade933bbc3cb4e8ce9a997b6b177
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-16 15:36:38 +0200

    Add project files.

---
+++ 8.B Exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.B_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(5, 4));
+            Console.WriteLine(test(4, 3));
+            Console.WriteLine(test(1, 4));
+            Console.ReadLine();
+        }
+        public static bool test(int x, int y) 
+        {
+            if (x == 5 || y == 5 ||(x + y == 5) ||Math.Abs(x - y) == 5 )
+                return true;
+            else
+                return false;
+        }
+    }
+}
